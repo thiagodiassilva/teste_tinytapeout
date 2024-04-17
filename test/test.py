ec8ecbb1e20964d009d7b140e7aa54e21bf7ff0b
Uri Shaked <uri@urishaked.com>
2024-04-17 21:44:17 +0300

    test: add comments to explain the test code structure

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
@@ -23,11 +23,18 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    # Set the input values, wait one clock cycle, and check the output
-    dut._log.info("Test")
+    dut._log.info("Test project behavior")
+
+    # Set the input values you want to test
     dut.ui_in.value = 20
     dut.uio_in.value = 30
 
+    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 50
+
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
