7f4b9158499bb80cf17d057d694e6a53addacd23
fabricchip <162960669+fabricchip@users.noreply.github.com>
2024-04-17 16:40:13 +0530

    Update tt_um_fpu.v

--- src/tt_um_fpu.v
+++ src/tt_um_fpu.v
@@ -5,27 +5,6 @@
 
 `default_nettype none
 
-module tt_um_fpu(
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-endmodule
-
-
-`timescale 1ns / 1ps
-
 module tt_2ip_mult(
 
     input  wire [7:0] ui_in,    // Dedicated inputs
