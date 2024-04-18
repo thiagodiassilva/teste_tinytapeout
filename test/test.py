e93011a61a83f599cc66dcb7335ad5ff33cdb85a
fabricchip <162960669+fabricchip@users.noreply.github.com>
2024-04-18 22:19:35 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,37 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: MIT
 
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
 
+import cocotb
+from cocotb.triggers import RisingEdge, Timer
+from cocotb.clock import Clock
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
+async def test_tt_um_fpu(dut):
+    # Start Clock
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
 
     # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
+    dut.rst_n.value = 0
+    dut.ena.value = 0
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await Timer(5, units="ns")
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Test input sequence
+    inputs = [
+        (0x40, 0x0c, 0xcc, 0xcd),
+        (0x40, 0x53, 0x33, 0x33),
+        (0x40, 0x8c, 0xcc, 0xcd),
+        (0x40, 0xb0, 0x00, 0x00)
+    ]
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    for byte_group in inputs:
+        for byte in byte_group:
+            await RisingEdge(dut.clk)
+            dut.ui_in.value = byte
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # Wait and finish the simulation
+    await Timer(100, units='ns')
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
